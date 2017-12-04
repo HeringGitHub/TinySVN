2cf6852507060784180a337a4d99110e00a1f0d8
hering <heringlinux@yeah.net>
2017-12-04 16:12:03 +0800

    Completed some display feture

--- ui.py
+++ ui.py
@@ -34,6 +34,20 @@ def recur_path(path, depth=0):
         for file in files:
             tree["child"][file] = recur_path(path + "/" + file, depth + 1)
     return tree
+
+def classify(filenames):
+    names = list(sorted(filenames))
+    count = len(names)
+    i = 0
+    while i < count:
+        if os.path.isfile(filenames[names[i]]["path"]):
+            names.append(names[i])
+            del names[i]
+            count = count - 1
+        else:
+            i = i + 1
+    return names
+
 class MainFrame(Frame):
     def __init__(self, bor=0):
         Frame.__init__(self, borderwidth=bor)
@@ -50,7 +64,9 @@ class MainFrame(Frame):
             index = index + 1
     def delitem(self, start=0, end=END):
         self.box.delete(start, end)
-
+    
+    def register_cb(self, func):
+        self.cb_func = func
 
 class MarkFrame(MainFrame):
     def __init__(self, bor=0):
@@ -59,41 +75,8 @@ class MarkFrame(MainFrame):
         self.dirlist = []
 
     def rollfolder(self, event):
-        index = self.box.curselection()[0]
-        childlist = self.dirlist[index]
-        dt = self.dirtree
-        info = {}
-        for c in childlist:
-            info = dt[c]
-            dt = info["child"]
-        
-        idx = index + 1
-        if info["list"]:
-            for c in info["list"]:
-                self.dirlist.insert(idx, c)
-                self.additem([c[-1]], idx, 2 * (len(c) - 1))
-                idx = idx + 1
-            info["closed"] = False
-            del info["list"][:]
-
-        elif info["child"]:
-            names = list(info["child"])
-            if info["closed"]:
-                for name in names:
-                    tmplist = list(childlist)
-                    tmplist.append(name)
-                    self.dirlist.insert(idx, tmplist)
-                    idx = idx + 1
-                self.additem(names, idx, 2 * (info["depth"] + 1))
-                info["closed"] = False
-            else:
-                while idx < len(self.dirlist) and len(self.dirlist[idx]) > len(childlist):
-                    info["list"].append(self.dirlist[idx])
-                    del self.dirlist[idx]
-                self.delitem(idx, idx + len(info))
-                info["closed"] = True
-        if info["child"]:
-            self.cb_func(info["child"])
+        self.index = self.box.curselection()[0]
+        self.update_cb()
 
     def addproj(self, name, path):
         self.dirtree[name] = recur_path(path)
@@ -103,10 +86,44 @@ class MarkFrame(MainFrame):
 
     def delproj(self, name):
         pass
-
-    def register_cb(self, func):
-        self.cb_func = func
     
+    def update_cb(self, index=-1):
+        childlist = self.dirlist[self.index + index + 1]
+        dt = self.dirtree
+        info = {}
+        for c in childlist:
+            info = dt[c]
+            dt = info["child"]
+        
+        idx = self.index + index + 1 + 1
+        if info["list"]:
+            for c in info["list"]:
+                self.dirlist.insert(idx, c)
+                self.additem([c[-1]], idx, 2 * (len(c) - 1))
+                idx = idx + 1
+            info["closed"] = False
+            del info["list"][:]
+
+        elif info["child"]:
+            names = classify(info["child"])
+            if info["closed"]:
+                tmpidx = idx
+                for name in names:
+                    tmplist = list(childlist)
+                    tmplist.append(name)
+                    self.dirlist.insert(tmpidx, tmplist)
+                    tmpidx = tmpidx + 1
+                self.additem(names, idx, 2 * (info["depth"] + 1))
+                info["closed"] = False
+            else:
+                while idx < len(self.dirlist) and len(self.dirlist[idx]) > len(childlist):
+                    info["list"].append(self.dirlist[idx])
+                    del self.dirlist[idx]
+                self.delitem(idx, idx + len(info["list"]) - 1)
+                info["closed"] = True
+        if info["child"] and index == -1:
+            self.cb_func(info["child"])
+
 class DirListFrame(MainFrame):
     def __init__(self, bor=0):
         MainFrame.__init__(self, bor=bor)
@@ -117,7 +134,7 @@ class DirListFrame(MainFrame):
     def update_cb(self, items):
         self.delitem()
         self.dirtree = items
-        self.dirlist = list(items.keys())
+        self.dirlist = classify(items)
         self.additem(self.dirlist)
 
     def rollfolder(self, event):
@@ -125,6 +142,7 @@ class DirListFrame(MainFrame):
         child = self.dirtree[self.dirlist[index]]["child"]
         if child:
             self.update_cb(child)
+            self.cb_func(index)
 class MainWindow(Tk):
     def __init__(self, master=None):
         Tk.__init__(self, master)
@@ -148,7 +166,7 @@ class MainWindow(Tk):
         self.DirDetail = DirListFrame(bor=1)
         self.DirDetail.place(relx=0.25, relwidth=0.75, relheight=1.0, bordermode=INSIDE)
         self.MarkBook.register_cb(self.DirDetail.update_cb)
-
+        self.DirDetail.register_cb(self.MarkBook.update_cb)
     def addproj(self, name, path):
         self.MarkBook.addproj(name, path)
 
